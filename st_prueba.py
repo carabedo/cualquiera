047e1e46123d455f86e85c81d56992af84f59a35
fernando <64593873+carabedo@users.noreply.github.com>
2023-08-01 20:58:09 -0300

    Add files via upload

---
+++ st_prueba.py
@@ -0,0 +1,20 @@
+import streamlit as st
+
+
+st.header('Elija las variables de la propiedad que quiere predecir:')
+
+
+
+barrio= st.selectbox(
+     'Barrio del inmueble?',['Gerli','Burzaco','Belgrano'])
+
+sup = st.slider('Superficie del inmueble?', 9, 400, 2)
+
+
+habs= st.slider('Cantidad de habitaciones?', 1, 10, 1)
+
+
+st.write('El precio aproximado por m2 es: ', 34)
+
+tipo = st.selectbox(
+     'Tipo de inmueble?',['ph','departamento','casa'])
